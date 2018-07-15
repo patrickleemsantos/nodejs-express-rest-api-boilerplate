09fcd3fcbd38a0f33ce23c7d9c314b4051837d10
Patrick Lee Santos <patrickleemsantos@gmail.com>
2018-07-15 22:31:59 +0800

    Initial commit

---
+++ models/user.js
@@ -0,0 +1,53 @@
+const config = require('config');
+const jwt = require('jsonwebtoken');
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        unique: true,
+        minlength: 5,
+        maxlength: 255
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 1024
+    },
+    isAdmin: Boolean
+});
+
+userSchema.methods.generateAuthToken = function() {
+    const token = jwt.sign({ _id: this._id, isAdmin: this.isAdmin }, config.get('jwtPrivateKey'));
+    return token;
+}
+
+const User = mongoose.model('User', userSchema);
+
+function validateUser(user) {
+    const schema = {
+        firstName: Joi.string().min(5).max(50).required(),
+        lastName: Joi.string().min(5).max(50).required(),
+        email: Joi.string().min(5).max(255).email().required(),
+        password: Joi.string().min(5).max(1024).required()
+    }
+
+    return Joi.validate(user, schema)
+}
+
+exports.User = User;
+exports.validate = validateUser;
