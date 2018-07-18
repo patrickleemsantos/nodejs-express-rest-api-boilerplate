a1c704b3a14cb6af74ff598da02eedce18688ddd
Patrick Lee Santos <patrickleemsantos@gmail.com>
2018-07-19 00:16:55 +0800

    Added helmet

--- startup/routes.js
+++ startup/routes.js
@@ -5,8 +5,8 @@ const auth = require('../routes/auth');
 const error = require('../middleware/error');
 
 module.exports = function (app) {
-    app.use(helmet());
     app.use(express.json());
+    app.use(helmet());
     app.use('/api/users', users);
     app.use('/api/auth', auth);
     app.use(error);
