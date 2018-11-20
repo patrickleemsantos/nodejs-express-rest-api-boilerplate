5df963869d14f8c83800f1d055af2617cc53d3e5
Patrick Lee Santos <patrickleemsantos@gmail.com>
2018-11-20 09:47:52 +0800

    Removed non-boilerplate pages

--- startup/routes.js
+++ startup/routes.js
@@ -2,7 +2,6 @@ const helmet = require('helmet');
 const express = require('express');
 const users = require('../routes/users');
 const auth = require('../routes/auth');
-const menu = require('../routes/menus');
 const error = require('../middleware/error');
 
 module.exports = function (app) {
@@ -10,6 +9,5 @@ module.exports = function (app) {
     app.use(helmet());
     app.use('/api/users', users);
     app.use('/api/auth', auth);
-    app.use('/api/menu', menu);
     app.use(error);
 }
